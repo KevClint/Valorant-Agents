333516312d7897fc7025e80a0823a5eac307a8fd
Clint Lorenzo <clintlorenzo11@gmail.com>
2025-08-02 20:47:06 +0800

    performance!!!!!!!!!

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-document.querySelector(".hamburger").addEventListener("click", function () {
-  document.querySelector(".nav-links").classList.toggle("open");
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
 });
